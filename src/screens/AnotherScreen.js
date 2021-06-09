3ad90da698b14b4c41cec726437d82c84035f020
Gruzinas1 <dgruzdzevicius@gmail.com>
2021-06-09 22:52:43 +0300

    passing aprams to previous screen

--- src/screens/AnotherScreen.js
+++ src/screens/AnotherScreen.js
@@ -1,15 +1,35 @@
-import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Button, TextInput} from 'react-native';
 
-function AnotherScreen({navigation}) {
+function AnotherScreen({navigation, route}) {
+  const [title, setTitle] = useState('');
+
+  const {name, age, id} = route.params;
   return (
     <View>
       <Text>Another Screen</Text>
+      <Text>Name: {name}</Text>
+      <Text>Age: {age}</Text>
+      <Text>Id: {id}</Text>
+      <TextInput
+        placeholder="Enter Title"
+        value={title}
+        onChangeText={setTitle}
+      />
       <Button
         title="To another screen"
         onPress={() => navigation.push('Another')}
       />
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button
+        title="Go to Home"
+        onPress={() => {
+          navigation.navigate({
+            name: 'Home',
+            params: {title},
+            merge: true,
+          });
+        }}
+      />
       <Button title="Go back" onPress={() => navigation.goBack()} />
       <Button
         title="Go back to first screen in stack"
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
 
-function HomeScreen({navigation}) {
+function HomeScreen({navigation, route}) {
+  console.log(route);
+  useEffect(() => {
+    if (route.params?.title) {
+      console.log('received params');
+    }
+  }, [route.params?.title]);
+
   return (
     <View>
       <Text>HomeScreen</Text>
       <Button
         title="To another screen"
-        onPress={() => navigation.navigate('Another')}
+        onPress={() =>
+          navigation.navigate('Another', {name: 'John', age: 26, id: 76})
+        }
       />
     </View>
   );
