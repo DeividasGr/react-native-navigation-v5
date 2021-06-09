a4db4f9db528d16703b849a4961d3b7484001c96
Gruzinas1 <dgruzdzevicius@gmail.com>
2021-06-09 23:22:36 +0300

    various header bar configurations

--- App.js
+++ App.js
@@ -3,19 +3,35 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from './src/screens/HomeScreen';
 import AnotherScreen from './src/screens/AnotherScreen';
+import LogoTitle from './src/components/LogoTitle';
 
 const Stack = createStackNavigator();
+
+const screenOptions = {
+  headerStyle: {backgroundColor: '#fd3fa8'},
+  headerTintColor: '#9f35e6fb',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+    letterSpacing: 10,
+  },
+};
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={screenOptions}>
         <Stack.Screen
           name="Home"
           component={HomeScreen}
-          options={{title: 'Welcome to Home Screen'}}
+          options={{
+            headerTitle: props => <LogoTitle {...props} />,
+          }}
         />
-        <Stack.Screen name="Another" component={AnotherScreen} />
+        <Stack.Screen
+          name="Another"
+          component={AnotherScreen}
+          options={({route}) => ({title: route.params.name})}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {View, Text, StyleSheet, Button, Image} from 'react-native';
 
 function HomeScreen({navigation, route}) {
   console.log(route);
@@ -15,13 +15,33 @@ function HomeScreen({navigation, route}) {
       <Button
         title="To another screen"
         onPress={() =>
-          navigation.navigate('Another', {name: 'John', age: 26, id: 76})
+          navigation.navigate('Another', {name: 'Mike', age: 26, id: 76})
+        }
+      />
+      <Button
+        title="Update the title"
+        onPress={() =>
+          navigation.setOptions({
+            headerTitle: props => (
+              <Image
+                style={styles.logo}
+                source={{
+                  uri: 'https://berenyisoft.eu/wp-content/uploads/2020/04/BSys-Smile.jpg',
+                }}
+              />
+            ),
+          })
         }
       />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  logo: {
+    width: 66,
+    height: 58,
+  },
+});
 
 export default HomeScreen;
